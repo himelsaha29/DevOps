d7602829991717c9fbbde09cff6736f245f91784
MarcSaber <marc.saber@mail.mcgill.ca>
2022-12-03 18:31:19 -0500

    round function added

--- src/main/java/com/ecse437/math/Round.java
+++ src/main/java/com/ecse437/math/Round.java
@@ -3,7 +3,14 @@ package com.ecse437.math;
 public class Round {
 
     public static int value(float a) {
-
-        return 0;
+        
+        int decimal = (int) a;
+        float fractional = a - decimal;
+        if (fractional < 0.5){
+            return decimal;
+        }
+        else{
+            return decimal + 1;
+        }
     }
 }
