c05d88b5f57319d8e0025ce53e54c44dc3ef199d
himelsaha29 <himelsaha29@live.com>
2022-12-04 13:43:57 -0500

    CelsiusToFahrenheit function and integration test added

---
+++ src/main/java/com/ecse437/math/CelsiusToFahrenheit.java
@@ -0,0 +1,9 @@
+package com.ecse437.math;
+
+public class CelsiusToFahrenheit {
+
+    public int convert(int a) {
+        return 0;
+    }
+}
+
---
+++ src/test/java/com/ecse437/project/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.ecse437.project;
+
+import com.ecse437.math.CelsiusToFahrenheit;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class IntegrationTest {
+    CelsiusToFahrenheit celsius = new CelsiusToFahrenheit();
+    @Test
+    void testCelsiusToFahrenheit() {
+        int celsiusValue = 18;
+        assertEquals(64, celsius.convert(celsiusValue));
+    }
+}
