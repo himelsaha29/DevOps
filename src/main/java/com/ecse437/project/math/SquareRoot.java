50c1ffa2921838b8456125195aa7292035ef96c0
himelsaha29 <himelsaha29@live.com>
2022-12-06 10:49:39 -0500

    SquareRoot implemented, test passing
    Change-Id: Ifa05d5fb064200e807708710c866da239de6d16f

--- src/main/java/com/ecse437/project/math/SquareRoot.java
+++ src/main/java/com/ecse437/project/math/SquareRoot.java
@@ -4,6 +4,17 @@ public class SquareRoot {
 
     public static double value(double a) {
 
-        return 0.0;
+        double lo = Math.min(1, a), hi = Math.max(1, a), mid = 0;
+
+        while(100 * lo * lo < a) lo *= 10;
+        while(0.01 * hi * hi > a) hi *= 0.1;
+
+        for(int i = 0 ; i < 100 ; i++){
+            mid = (lo+hi)/2;
+            if(mid*mid == a) return mid;
+            if(mid*mid > a) hi = mid;
+            else lo = mid;
+        }
+        return mid;
     }
 }
--- src/test/java/com/ecse437/project/IntegrationTest.java
+++ src/test/java/com/ecse437/project/IntegrationTest.java
@@ -1,6 +1,6 @@
 package com.ecse437.project;
 
-import com.ecse437.project.math.CelsiusToFahrenheit;
+
 import com.ecse437.project.math.CelsiusToFahrenheit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
