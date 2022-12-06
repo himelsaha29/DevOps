b7201764ad376e2e2cbf59097361b0dfcafec540
himelsaha29 <himelsaha29@live.com>
2022-12-06 18:27:35 -0500

    Coordinate distance added, implemented, passing integration test
    Change-Id: If7397ccd17c237f792ecb1f66456b173759bbcb7

--- src/test/java/com/ecse437/project/IntegrationTest.java
+++ src/test/java/com/ecse437/project/IntegrationTest.java
@@ -2,6 +2,7 @@ package com.ecse437.project;
 
 import com.ecse437.project.math.CelsiusToFahrenheit;
 import com.ecse437.project.math.CelsiusToFahrenheit;
+import com.ecse437.project.math.CoordinateDistance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,9 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class IntegrationTest {
     CelsiusToFahrenheit celsius = new CelsiusToFahrenheit();
+    CoordinateDistance coordinateDistance = new CoordinateDistance();
     @Test
     void testCelsiusToFahrenheit() {
         int celsiusValue = 18;
         assertEquals(64, celsius.convert(celsiusValue));
+    }
+
+    @Test
+    void testCoordinateDistance() {
+        assertEquals(2, coordinateDistance.value(1,2,4,6));
     }
 }
