378c8edf0eba59322c7535bbf94e425ba2f2100e
himelsaha29 <himelsaha29@live.com>
2022-12-06 20:35:31 -0500

    CelsiusToFahrenheit bug fixed
    Change-Id: I3f6c3bc007dbe7cfe6e48d626cd34d9174553c47

--- src/main/java/com/ecse437/project/math/CelsiusToFahrenheit.java
+++ src/main/java/com/ecse437/project/math/CelsiusToFahrenheit.java
@@ -3,16 +3,8 @@ package com.ecse437.project.math;
 public class CelsiusToFahrenheit {
 
     public int convert(int a) {
-        int constant1 = 32;
-        int constant2 = 64;
 
-        int fahrenheit = 0;
-        int celsius = a;
-
-        fahrenheit = (int) AddExact.values((long)(a*1.8), constant1);
-
-
-        return fahrenheit;
+        return (int) AddExact.values((long)(a*1.8), 32);
     }
 }
 
